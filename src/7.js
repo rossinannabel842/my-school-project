bb8fe99c47a1abcf0ea78b37bb887d2b35a48488
rossinannabel842 <rossinannabel842@gmail.com>
2025-03-12 05:39:25 +0000

    Add 7.js

---
+++ src/7.js
@@ -0,0 +1,8 @@
+// my-school-project/my-school-project.js
+
+function calculateBmi(weight, height) {
+  const bmi = weight / (height * height);
+  return bmi;
+}
+
+console.log(calculateBmi(70, 1.8));
